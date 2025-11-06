c7ff0bd785b9c3b9587ab73090a10477d9e1e30f
Golovko, Yaroslav <golovko085@gmail.com>
2025-11-06 23:16:54 +0700

    Inputting and database

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -18,7 +20,7 @@ func (events *Events) GetEventsForToday() *Events {
 	eventsForToday := &Events{}
 
 	for _, event := range events.Events {
-		if event.Date.Format("02/01/2006") == time.Now().Format("02/01/2006") {
+		if event.Date.Format("01/02/2006") == time.Now().Format("01/02/2006") {
 			eventsForToday.Events = append(eventsForToday.Events, event)
 		}
 	}
@@ -30,7 +32,7 @@ func (events *Events) GetAllEventsOnFuture() *Events {
 	eventsOnFuture := &Events{}
 
 	for _, event := range events.Events {
-		if event.Date.Sub(time.Now()) > time.Hour*24 {
+		if time.Until(event.Date) > time.Hour*24 {
 			eventsOnFuture.Events = append(eventsOnFuture.Events, event)
 		}
 	}
@@ -77,6 +79,43 @@ func InitDb() error {
 	return nil
 }
 
+func GetEventsDb() (*Events, error) {
+	events := &Events{}
+
+	rows, err := db.Query("SELECT name, place, date FROM events")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		event := Event{}
+		strDate := ""
+		rows.Scan(&event.Name, &event.Place, &strDate)
+		event.Date, err = time.Parse("01/02/2006 03:04", strDate)
+		if err != nil {
+			return nil, err
+		}
+
+		events.Events = append(events.Events, event)
+	}
+
+	return events, nil
+}
+
+func SetEventsDb(events *Events) error {
+	for _, event := range events.Events {
+		_, err := db.Exec("INSERT INTO events (name, place, date) VALUES (:name, :place, :date)",
+			sql.Named("name", event.Name),
+			sql.Named("place", event.Place),
+			sql.Named("date", event.Date.Format("01/02/2006 03:04")))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func Close() error {
 	return db.Close()
 }
@@ -86,51 +125,58 @@ func main() {
 		log.Fatal(err)
 	}
 
-	events := Events{
-		Events: []Event{
-			{
-				Name:  "Go for a work",
-				Place: "Office",
-				Date:  time.Now(),
-			},
-			{
-				Name:  "Go for a walk",
-				Place: "Park",
-				Date:  time.Now(),
-			},
-			{
-				Name:  "Go for a work",
-				Place: "Office",
-				Date:  time.Date(2005, time.January, 1, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				Name:  "Go for a walk",
-				Place: "Park",
-				Date:  time.Date(2005, time.January, 1, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				Name:  "Go for a walk",
-				Place: "Park",
-				Date:  time.Date(2005, time.January, 1, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				Name:  "Go for a library",
-				Place: "Library",
-				Date:  time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC),
-			},
-			{
-				Name:  "Go for a club",
-				Place: "Club",
-				Date:  time.Date(2026, time.January, 1, 0, 0, 0, 0, time.UTC),
-			},
-		},
+	events, err := GetEventsDb()
+	if err != nil {
+		log.Fatal(err)
 	}
+	mode := ""
 
-	fmt.Println("Hello, I am simple notifier.")
-	fmt.Println("Events for today:")
-	fmt.Println(events.GetEventsForToday().Events)
-	fmt.Println("All events on future:")
-	fmt.Println(events.GetAllEventsOnFuture())
-	fmt.Println("All events:")
-	fmt.Println(events.Events)
+	reader := bufio.NewReader(os.Stdin)
+
+	for mode != "exit" {
+		fmt.Print("Input mode: ")
+		fmt.Scanln(&mode)
+
+		switch mode {
+		case "add":
+			var name, place, date string
+			fmt.Print("Input name: ")
+			fmt.Scanln(&name)
+			fmt.Print("Input place: ")
+			fmt.Scanln(&place)
+			fmt.Print("Input date(01/02/2006 03:04): ")
+			//fmt.Scanf("%s %s", &date, &time_s)
+			date, err = reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			date = strings.TrimSpace(date)
+
+			time, err := time.Parse("01/02/2006 03:04", date)
+			if err != nil {
+				log.Fatal(err)
+			}
+			events.Events = append(events.Events, Event{name, place, time})
+		case "get":
+			fmt.Println("All events: ")
+			for _, event := range events.Events {
+				fmt.Println(event)
+			}
+
+			fmt.Println("Events for today: ")
+			for _, event := range events.GetEventsForToday().Events {
+				fmt.Println(event)
+			}
+
+			fmt.Println("Events on future: ")
+			for _, event := range events.GetAllEventsOnFuture().Events {
+				fmt.Println(event)
+			}
+
+		default:
+			fmt.Println("Unknown command.")
+		}
+
+		mode = ""
+	}
 }
